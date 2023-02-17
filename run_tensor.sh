a0ff62fccc42cea8967f749dca7a9b8ea79dd310
olokevin <yequan_zhao@ucsb.edu>
2023-02-17 04:17:13 +0000

    OK to train

---
+++ run_tensor.sh
@@ -0,0 +1,8 @@
+# CUDA_VISIBLE_DEVICES=1 python -u tensor_train.py -tensorized 1 -uncompressed 0 -precondition 0 -n_warmup_steps 50000 -data data/multi30k.atok.low.pt -save_model model/test_ATIS_tensor_2layer_INT4 -save_mode best -proj_share_weight -label_smoothing -batch_size 64 -epoch 500|tee logs/test_ATIS_tensor_2layer_INT4.txt
+
+# file=ATIS_tensor_2layers_FP16_all510_fix_1e-3_r20_newshape
+# file=ATIS_notensor_2layers_FP32
+file=ATIS_tensor_2layers_FP32
+
+
+CUDA_VISIBLE_DEVICES=2 python -u tensor_train.py -tensorized 1 -uncompressed 0 -precondition 0 -save_model model/${file} -save_mode best -proj_share_weight -label_smoothing -batch_size 32 -epoch 500|tee logs/${file}.txt
--- tensor_layers/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name = 'tensor_layers',
-    packages = find_packages(),
-    version = '0.1',
-)
